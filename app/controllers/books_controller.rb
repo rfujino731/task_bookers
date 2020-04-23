3399a898189e35a57cc454899a9cce4059a8130c
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-04-23 17:49:37 +0900

    add_create_view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,18 @@
 class BooksController < ApplicationController
+	def new
+		# 投稿するために空のモデルオブジェクトを生成する
+		@book = Book.new
+	end
+
+	def create
+		book = Book.new(book_params)
+		book.save
+		redirect_to '/top'
+	end
+
+	private
+	
+	def book_params
+		params.require(:book).permit(:title, :body)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+
+  get 'new' => 'books#new'
+
+  post 'books' => 'books#create'
 end
