1f119e5e521e867f68fd9e5444713ce236c03c67
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-04-25 20:50:24 +0900

    add error_mesage modified controller renders

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,9 +7,13 @@ class BooksController < ApplicationController
     end
 
     def create
-		book = Book.new(book_params)
-		book.save
-		redirect_to '/books'
+		@book = Book.new(book_params)
+		if @book.save
+		   redirect_to books_path
+		 else
+		   @books = Book.all
+		   render :index
+		end
 	end
 
 	def show
