827f3f84c29f3dc37f24c049650d46aa11a9bcdb
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-04-25 13:14:33 +0900

    add_edit_destory

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,14 @@ Rails.application.routes.draw do
   get  'books' => 'books#index'
   #投稿後のアクション
   post 'books' => 'books#create'
-  
+  #詳細画面
   get 'books/:id' => 'books#show', as: 'book'
+  #編集画面
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  #編集後の保存
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  #投稿の削除
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   #newは、投稿時のページ
   # get 'new' => 'books#new'
 end
